ae4615c31df349853f80c60a50d2c76dda788b93
ryandefea <ryan.defea@gmail.com>
2022-06-03 12:10:13 -0700

    create const for gender in js.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
   $("form#languagematch").submit(function(event){
 
     const age = parseInt($("input#age").val());
-    
+    const gender = $("select#gender").val();
+    const 
+
 
   })
 
